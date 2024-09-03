a57ba48a90f79093c6bdee3a24ebfebe523624df
JulienEsposito <127495162+JulienEsposito@users.noreply.github.com>
2024-09-03 22:41:25 +0200

    Delete src/config_private_instruction.py

--- src/config_private_instruction.py
+++
@@ -1,5 +0,0 @@
-API_KEY = '' # CryptoCompare API key
-BASE_URL = '' # CryptoCompare URL
-TELEGRAM_TOKEN = '' # Telegram token of your bot
-CHAT_ID = '' # Chat ID of your telegram bot
-json_file = ''
