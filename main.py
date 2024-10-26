6b3c69132bc02247681cb3580ae0326e325b075e
JulienEsposito <julienhand94@hotmail.fr>
2024-10-26 23:57:42 +0200

    fix: use github actions secrets

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.config import CURRENCY
-from src.config_private import STOCK
+#from src.config_private import STOCK
 from src.external_data import get_stock_prices
 from src.sell import check_stock_price_range
 
--- src/sell.py
+++ src/sell.py
@@ -3,7 +3,7 @@ import json
 from collections import defaultdict
 
 from src.config import bot
-from src.config_private import json_file, CHAT_ID
+#from src.config_private import json_file, CHAT_ID
 from src.external_data import exit_target, get_portfolio_sheet
 
 
