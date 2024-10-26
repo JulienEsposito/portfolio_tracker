35fce2111e47b73a1dd942f13616c4f0ca9fe8b3
JulienEsposito <julienhand94@hotmail.fr>
2024-10-27 00:33:44 +0200

    fix: typo

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ CURRENCY = "USD"
 API_KEY = os.getenv('API_KEY')
 BASE_URL = os.getenv('BASE_URL')
 CHAT_ID = os.getenv('CHAT_ID')
-JSON_FILE = os.getenv('JSON_FILE')
+json_file = os.getenv('JSON_FILE')
 STOCK = os.getenv('STOCK')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
