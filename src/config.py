71e986163c8ce197accf773a9468844e66515ba9
JulienEsposito <127495162+JulienEsposito@users.noreply.github.com>
2024-10-26 23:24:42 +0200

    feat: update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,14 @@
 import telebot
-from src.config_private import TELEGRAM_TOKEN
+#from src.config_private import TELEGRAM_TOKEN
 
 CURRENCY = "USD"
+
+API_KEY = os.getenv('API_KEY')
+BASE_URL = os.getenv('BASE_URL')
+CHAT_ID = os.getenv('CHAT_ID')
+JSON_FILE = os.getenv('JSON_FILE')
+STOCK = os.getenv('STOCK')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
