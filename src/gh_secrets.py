dfdeafbcaab814e758010af31166c75df21a04a8
JulienEsposito <julienhand94@hotmail.fr>
2025-01-18 16:32:16 +0100

    feat: Deleting the github secret 'json_file'

--- src/gh_secrets.py
+++ src/gh_secrets.py
@@ -3,7 +3,6 @@ import os
 API_KEY = os.getenv('API_KEY')
 BASE_URL = os.getenv('BASE_URL')
 CHAT_ID = os.getenv('CHAT_ID')
-#json_file = os.getenv('JSON_FILE')
 STOCK = os.getenv('STOCK')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
--- src/gh_secrets_instructions.py
+++ src/gh_secrets_instructions.py
@@ -3,4 +3,3 @@ BASE_URL = '' # Stock tracker URL
 TELEGRAM_TOKEN = '' # Telegram token of your bot
 CHAT_ID = '' # Chat ID of your telegram bot
 STOCK = '' # Stock to track
-#json_file = ''
