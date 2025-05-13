df928ec8688bddc5a531ea0fd5856d4768b75c69
JulienEsposito <julienhand94@hotmail.fr>
2025-05-13 22:54:46 +0200

    feat: automate stocks sourcing

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from portfolio_tracker.external_data import get_stock_prices, get_stocks
 from portfolio_tracker.sell import check_stock_price_range
 
 stocks = get_stocks()
-df_stock = get_stock_prices(STOCK, CURRENCY)
+df_stock = get_stock_prices(stocks, CURRENCY)
 check_stock_price_range(df_stock)
 
